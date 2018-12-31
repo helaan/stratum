a75469c3ae6c4f26d099c1bbf49dbb421fa5b6b0
Henk van der Laan <opensource@henkvdlaan.com>
2018-12-31 16:16:29 +0100

    Implement nicer way to query database

--- src/controllers/team.rs
+++ src/controllers/team.rs
@@ -4,10 +4,10 @@
 //! - Edit teams
 //! - Remove teams
 
-use actix_web::{error, AsyncResponder, Form, HttpRequest, Responder};
+use actix_web::{error, AsyncResponder, Error, Form, HttpRequest, Responder};
 use actix::prelude::*;
 use crate::{AppState, util::render};
-use crate::database::DbExecutor;
+use crate::database::{Execute, DbExecutor};
 use tera::{Context};
 use crate::schema::teams;
 use crate::models::Team;
@@ -42,7 +42,12 @@ pub fn create_form(req: HttpRequest<AppState>) -> impl Responder {
 }
 
 pub fn create(req: HttpRequest<AppState>, form: Form<CreateTeam> )  -> impl Responder {
-    req.state().db.send(form.into_inner()).from_err().and_then(move |res| match res {
+    //req.state().db.send(form.into_inner()).from_err().and_then(move |res| match res {
+    req.state().db.send(Execute::new(|s| -> Result<usize, Error> {
+        let conn = s.get_conn()?;
+        diesel::insert_into(teams::table).values(&form.into_inner()).execute(&conn)
+            .map_err(|_| error::ErrorInternalServerError("Error inserting team"))
+    })).from_err().and_then(move |res| match res {
         Ok(_) => {
             Ok("succesfully added team")
         }
@@ -50,23 +55,12 @@ pub fn create(req: HttpRequest<AppState>, form: Form<CreateTeam> )  -> impl Resp
     }).responder()
 }
 
-pub struct AllTeams;
-
-impl Message for AllTeams {
-    type Result = Result<Vec<Team>, actix_web::Error>;
-}
-
-impl Handler<AllTeams> for DbExecutor {
-    type Result = Result<Vec<Team>,actix_web::Error>;
-    fn handle(&mut self, _: AllTeams, _: &mut Self::Context) -> Self::Result {
-        let conn = self.get_conn()?;
-        teams::dsl::teams.order(teams::id.asc()).load::<Team>(&conn)
-            .map_err(|e| error::ErrorInternalServerError(e))
-    }
-}
-
 pub fn index(req: HttpRequest<AppState>) -> impl Responder {
-    req.state().db.send(AllTeams).from_err().and_then(move |res| match res {
+    req.state().db.send(Execute::new(|s| -> Result<Vec<Team>, Error> {
+        let conn = s.get_conn()?;
+        teams::dsl::teams.order(teams::id.asc()).load(&conn)
+            .map_err(|e| error::ErrorInternalServerError(e))
+    } )).from_err().and_then(move |res : Result<Vec<Team>, Error>| match res {
         Ok(teams) => {
             let mut ctx = Context::new();
             ctx.insert("teams", &teams);
--- src/database.rs
+++ src/database.rs
@@ -2,6 +2,7 @@ use actix::prelude::*;
 use actix_web::{error, Error};
 use diesel::PgConnection;
 use diesel::r2d2::{ConnectionManager, Pool, PoolError, PooledConnection};
+use std::marker::Send;
 
 type DbPool = Pool<ConnectionManager<PgConnection>>;
 type DbConnection = PooledConnection<ConnectionManager<PgConnection>>;
@@ -23,11 +24,52 @@ impl Actor for DbExecutor {
     type Context = SyncContext<Self>;
 }
 
-struct Query {
-    query: str,
+/* Largely copied from Actix:
+ * https://github.com/actix/actix/blob/master/src/msgs.rs
+ * Copy needed to add DbExecutor parameter to message
+ */
+pub struct Execute<I: Send + 'static = (), E: Send + 'static = ()>(Box<FnExec<I, E>>);
+impl<I: Send, E: Send> Message for Execute<I, E> {
+    type Result = Result<I, E>;
 }
 
-impl Message for Query {
-    type Result = Result<String, Error>;
+impl<I, E> Execute<I, E>
+where
+    I: Send + 'static,
+    E: Send + 'static,
+{
+    pub fn new<F>(f: F) -> Self
+    where
+        F: FnOnce(&&mut DbExecutor) -> Result<I, E> + Send + 'static,
+    {
+        Execute(Box::new(f))
+    }
+
+    /// Execute enclosed function
+    pub fn exec(self, s: &&mut DbExecutor) -> Result<I, E> {
+        self.0.call_box(s)
+    }
 }
 
+trait FnExec<I: Send + 'static, E: Send + 'static>: Send + 'static {
+    fn call_box(self: Box<Self>, s: &&mut DbExecutor) -> Result<I, E>;
+}
+
+impl<I, E, F> FnExec<I, E> for F
+where
+    I: Send + 'static,
+    E: Send + 'static,
+    F: FnOnce(&&mut DbExecutor) -> Result<I, E> + Send + 'static,
+{
+    #[cfg_attr(feature = "cargo-clippy", allow(boxed_local))]
+    fn call_box(self: Box<Self>, s: &&mut DbExecutor) -> Result<I, E> {
+        (*self)(s)
+    }
+}
+
+impl<I: Send, E: Send> Handler<Execute<I, E>> for DbExecutor {
+    type Result = Result<I, E>;
+    fn handle(&mut self, ex: Execute<I, E>, _: &mut Self::Context) -> Result<I, E> {
+        ex.exec(&self)
+    }
+}
