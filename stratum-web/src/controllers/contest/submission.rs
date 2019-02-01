1346e6dc9130d141a7b8ba6cef8bbb377aee5351
Henk van der Laan <opensource@henkvdlaan.com>
2019-02-01 18:28:42 +0100

    Add submission details on frontend

--- stratum-web/src/controllers/contest/submission.rs
+++ stratum-web/src/controllers/contest/submission.rs
@@ -2,20 +2,25 @@ use crate::multipart::parse_multipart;
 use crate::util::render;
 use crate::AppState;
 use actix_web::{
-    error, http::Method, AsyncResponder, Error, HttpMessage, HttpRequest, Responder, Scope,
+    error, http::Method, AsyncResponder, Error, HttpMessage, HttpRequest, Path, Responder, Scope,
 };
+use chrono::{DateTime, Utc};
 use diesel::prelude::*;
 use diesel::r2d2::{ConnectionManager, PooledConnection};
 use futures::future::Future;
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
-use stratum_db::models::{Contest, Judgement, Problem, Submission, Team};
-use stratum_db::schema::{contest_problems, judgements, problems, submission_files, submissions};
+use stratum_db::models::{Contest, Judgement, Problem, Submission, Team, TestCaseJudgement};
+use stratum_db::schema::{
+    contest_problems, judgements, problems, submission_files, submissions, test_case_judgements,
+};
 use stratum_db::Execute;
 use tera::Context;
 
 pub fn register(scop: Scope<AppState>) -> Scope<AppState> {
     scop.route("", Method::GET, index)
         .route("/new", Method::POST, create)
+        .route("/{location_id:\\d+}/{id:\\d+}", Method::GET, show)
 }
 
 pub fn index(req: HttpRequest<AppState>) -> impl Responder {
@@ -141,6 +146,86 @@ pub fn create(req: HttpRequest<AppState>) -> impl Responder {
         .from_err()
         .and_then(|res| match res {
             Ok(n) => Ok(format!("uploaded {} submissions", n)),
+            Err(e) => Err(e),
+        })
+        .responder()
+}
+
+#[derive(Deserialize)]
+pub struct IdLocationIdParams {
+    id: i64,
+    location_id: i32,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Utf8TestCaseJudgement {
+    pub judgement_id: i64,
+    pub judgement_grader_id: i32,
+    pub test_case_position: i32,
+    pub status_code: i32,
+    pub output: String,
+    pub error: String,
+    pub created_at: DateTime<Utc>,
+}
+
+fn show(req: HttpRequest<AppState>, params: Path<IdLocationIdParams>) -> impl Responder {
+    let team = req
+        .extensions()
+        .get::<Team>()
+        .ok_or_else(|| {
+            error::ErrorForbidden("You are not allowed to submit as you are not in a team")
+        })
+        .map(|t| t.id);
+    req.state()
+        .db
+        .send(Execute::new(move |conn| -> Result<_, Error> {
+            let sub = submissions::table
+                .find((params.location_id, params.id))
+                .inner_join(problems::table)
+                .first::<(Submission, Problem)>(&conn)
+                .map_err(error::ErrorInternalServerError)?;
+            let team_id = team?;
+            if sub.0.team_id != team_id {
+                return Err(error::ErrorNotFound("Could not find this submission"));
+            }
+            let jm = judgements::table
+                .filter(judgements::submission_id.eq(params.id))
+                .filter(judgements::submission_location_id.eq(params.location_id))
+                .filter(judgements::valid.eq(true))
+                .order(judgements::grader_id.asc())
+                .first::<Judgement>(&conn)
+                .map_err(error::ErrorInternalServerError)?;
+            let tcjs = test_case_judgements::table
+                .filter(test_case_judgements::judgement_id.eq(jm.id))
+                .filter(test_case_judgements::judgement_grader_id.eq(jm.grader_id))
+                .order(test_case_judgements::test_case_position.asc())
+                .load::<TestCaseJudgement>(&conn)
+                .map(|v| {
+                    v.into_iter()
+                        .map(|tcj| Utf8TestCaseJudgement {
+                            judgement_id: tcj.judgement_id,
+                            judgement_grader_id: tcj.judgement_grader_id,
+                            test_case_position: tcj.test_case_position,
+                            status_code: tcj.status_code,
+                            output: String::from_utf8(tcj.output).unwrap(),
+                            error: String::from_utf8(tcj.error).unwrap(), //HACK
+                            created_at: tcj.created_at,
+                        })
+                        .collect::<Vec<_>>()
+                })
+                .map_err(error::ErrorInternalServerError)?;
+            Ok((sub, jm, tcjs))
+        }))
+        .from_err()
+        .and_then(move |res| match res {
+            Ok((sub, jm, tcjs)) => {
+                let mut ctx = Context::new();
+                ctx.insert("submission", &sub.0);
+                ctx.insert("problem", &sub.1);
+                ctx.insert("judgement", &jm);
+                ctx.insert("test_case_judgements", &tcjs);
+                render(&req, "contest/submission/show.html", ctx)
+            }
             Err(e) => Err(e),
         })
         .responder()
